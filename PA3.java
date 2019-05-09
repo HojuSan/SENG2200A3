19e271f29d11a1042d92943dae07e4f98a0f4aad
juyong kim <Juyong.Kim@uon.edu.au>
2019-05-09 12:28:20 +1000

    second commit

---
+++ PA3.java
@@ -0,0 +1,15 @@
+/*
+Title:              Assignment3 PA3.java
+Course:             SENG2200
+Author:             Juyong Kim
+Student No:         c3244203
+Date:               09/05/2019
+Description:        
+*/
+public class PA3
+{
+    public static void main(String[] args) throws IOException 
+    {
+        System.out.println("running");
+    }
+}
