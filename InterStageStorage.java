a005b6b3c1ade3a063bc386848698f2fa1999d2b
juyong kim <Juyong.Kim@uon.edu.au>
2019-05-21 11:51:51 +1000

    need to create timeStamps to properly utilize everything

--- InterStageStorage.java
+++ InterStageStorage.java
@@ -10,6 +10,7 @@ import java.util.Queue;
 import java.util.LinkedList;
 import java.util.List;
 
+//fifo method is being used for this class
 public class InterStageStorage
 {
     private int maxQ;
@@ -39,6 +40,31 @@ public class InterStageStorage
         this.countQ = new LinkedList<Integer>();
     }
 
+    //adds to the top of the queue
+    public boolean enqueue(Item newItem, double currentTime)
+    {
+        if(que.size())
+        {
+            newItem.setEntryTime(currentTime);
+            this.count += 1;
+            return que.add(newItem);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    //
+    public Item deque(double currentTime)
+    {
+        Item deque = que.poll();                              //poll is queue function, removes the head of the list
+        deque.setExitTime(currentTime);
+        //working on this, time stamps are important
+        this.timeQ.add(deque.queueStampTime(this.name));
+        this.count -= 1;
+        return deque;
+    }
+
     public boolean checkFull()
     {
         if(this.que.size()<this.maxQ)
@@ -64,15 +90,13 @@ public class InterStageStorage
         }
     }
 
-    public boolean enqueue(Item newItem, double currentTime)
+    //getters
+    public String getName()
     {
-        if(que.size())
-        {
-
-        }
-        else
-        {
-            return false;
-        }
+        return this.name;
+    }
+    public int getCount()
+    {
+        return this.count;
     }
 }
