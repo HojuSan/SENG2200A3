dd474d7c3df9cb3798c5557be7f9d2387372a0f0
Hoju <ghwntks3@gmail.com>
2019-05-22 11:53:29 +1000

    making average calculations for storage

--- InterStageStorage.java
+++ InterStageStorage.java
@@ -58,11 +58,16 @@ public class InterStageStorage
     public Item deque(double currentTime)
     {
         Item deque = que.poll();                              //poll is queue function, removes the head of the list
-        deque.setExitTime(currentTime);
+        deque.setExitTime(currentTime);                       //but returns the value  
         //working on this, time stamps are important
         this.timeQ.add(deque.queueStampTime(this.name));
         this.count -= 1;
         return deque;
+    }
+    //keeps track of the amounts for average calculation
+    public void stampCount()
+    {
+        this.countQ.add(new Integer(this.count));
     }
 
     public boolean checkFull()
@@ -98,5 +103,30 @@ public class InterStageStorage
     public int getCount()
     {
         return this.count;
+    }
+
+    //calculates average time an item is within the queue
+    public double getIsqQueueTime()
+    {
+        double totalItems = this.qStamp.size();
+        double totalDuration = 0;
+        for (TimeStamp t : this.qStamp)
+        {
+            totalDuration += t.getDuration();
+        }
+        return (totalItems==0)?0:totalDuration/totalItems;
+    }
+
+    //calculates average number of items in the queue at anytime
+    //utilizes time stamps
+    public double getIsqAvgItemCount()
+    {
+        double totalStamps = this.countStamp.size();
+        double totalCount = 0;
+        for (Integer i : this.countStamp)
+        {
+            totalCount += i;
+        }
+        return (totalStamps==0)?0:(totalCount/totalStamps);
     }
 }
